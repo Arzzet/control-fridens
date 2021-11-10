c5bcee95571d81094cb0c55da11a902fbb5a3180
Iwo <iwoskater3@hotmail.com>
2021-11-10 15:22:31 +0100

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoardComponent } from './componentes/board/board.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { EditarFridenComponent } from './componentes/editar-friden/editar-friden.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { NotFoundComponent } from './componentes/not-found/not-found.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BoardComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registrarse', component: RegistroComponent},
+  {path: 'configuracion', component: ConfiguracionComponent},
+  {path: 'friden/editar/:id', component: EditarFridenComponent},
+  {path: '**', component: NotFoundComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
